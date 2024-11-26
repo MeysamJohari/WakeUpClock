12b510ccddbc737a582ff91cad8e4b23b1d4eeeb
SPN <mr.johari67@gmail.com>
2024-11-26 21:00:35 +0330

    a while loop to showing timer

--- WakeupClock.py
+++ WakeupClock.py
@@ -1,10 +1,18 @@
 import winsound
 import time
+t = int(input("Enter the time in seconds: "))
+while t > 0:
+    min , sec = divmod(t,60)
+    timer = str(min) + ":" + str(sec)
+    print(timer, end="\r ")
+    time.sleep(1)
+    t -= 1
+
 def wakeup():
     for i in range(5):
         for j in range(2):
             winsound.Beep(400,200)
         time.sleep(1)    
-    
+
 wakeup()
     
